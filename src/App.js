f729310eb628839574a53f2e50dcec3b6bacb361
Vitaly <ivanov.arduan@yandex.ru>
2023-08-18 15:07:32 +0300

    Component add.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 
+
+function MyComponent() {
+  return (
+      <div>
+        <h1>Hello from the reusable component</h1>
+        <button>Like!</button>
+      </div>
+  )
+
+}
+
 function App() {
   return (
     <div className="App">
-
+      <MyComponent />
+      <MyComponent />
+      <MyComponent />
+      <MyComponent />
     </div>
   );
 }
