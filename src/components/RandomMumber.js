8dcd8badeeef11bffbd409ce22d06abacb34e3d4
Vitaly <ivanov.arduan@yandex.ru>
2023-09-02 12:39:45 +0300

    generateRandomNum.js add.

--- src/components/RandomMumber.js
+++ src/components/RandomMumber.js
@@ -1,9 +1,15 @@
+import {useState} from "react";
+import generateRandomNum from "../utils/generateRandomNum";
 
 function RandomMumber(){
+    const [randomNum, serRandomNum] = useState(generateRandomNum())
+    const changeRandomMum = () => {
+        serRandomNum(generateRandomNum())
+    }
     return(
         <div>
-            <h1>100</h1>
-            <button>Genetate new random number</button>
+            <h1>{randomNum}</h1>
+            <button onClick={changeRandomMum}>Genetate new random number</button>
         </div>
     )
 }
